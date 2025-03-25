9dee55784a29198eb8d9c3390f5ca4e449f3a644
Wang, Yang <yang4.wang@intel.com>
2025-03-25 09:32:28 +0800

    enable to record the memory into csv file.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include <chrono>
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
 #include <openvino/op/ops.hpp>
 #include <openvino/openvino.hpp>
 #include <openvino/runtime/auto/properties.hpp>
@@ -26,6 +29,8 @@ int main(int argc, char* argv[]) {
         std::cerr << "Error: Device name or model paths cannot be empty. Please provide valid inputs." << std::endl;
         return -1;
     }
+    std::ostringstream csv_filename;
+    csv_filename << device_name << "_";
     ov::Core core;
     std::cout << "OpenVINO version: " << ov::get_openvino_version() << std::endl;
     std::cout << "Device Name: " << device_name << std::endl;
@@ -38,23 +43,38 @@ int main(int argc, char* argv[]) {
     // Example: Compile the first model
     if (!model_path1.empty()) {
         std::cout << "Model Path 1: " << model_path1 << std::endl;
+        csv_filename << model_path1.substr(model_path1.find_last_of("/\\") + 1) << "_";
         model_1 = core.read_model(model_path1);
         compiled_model_1 = core.compile_model(model_1, device_name, properties);
         std::cout << "Model 1 compiled successfully." << std::endl;
     }
     if (!model_path2.empty()) {
         std::cout << "Model Path 2: " << model_path2 << std::endl;
+        csv_filename << model_path2.substr(model_path2.find_last_of("/\\") + 1) << "_";
         model_2 = core.read_model(model_path2);
         compiled_model_2 = core.compile_model(model_2, device_name, properties);
         std::cout << "Model 2 compiled successfully." << std::endl;
     }
     if (!model_path3.empty()) {
         std::cout << "Model Path 3: " << model_path3 << std::endl;
+        csv_filename << model_path3.substr(model_path3.find_last_of("/\\") + 1) << "_";
         model_3 = core.read_model(model_path3);
         compiled_model_3 = core.compile_model(model_3, device_name, properties);
         std::cout << "Model 3 compiled successfully." << std::endl;
     }
+    csv_filename << "memory_footprint.csv";
+    std::string csv_file_path = csv_filename.str();
+    std::replace(csv_file_path.begin(), csv_file_path.end(), '/', '_');
     std::cout << "Will start inference on model 1 with number of iterations: " << niters << std::endl;
+    std::cout << "and write memory usage data to: " << csv_file_path << std::endl;
+
+    std::ofstream csv_file(csv_file_path);
+    if (!csv_file.is_open()) {
+        std::cerr << "Error: Unable to open file for writing memory usage data." << std::endl;
+        return -1;
+    }
+    // Write CSV header
+    csv_file << "Iteration,Commit Size (MB),Heap Size (MB)" << std::endl;
     try {
         // will implement inference on model_1 only
         if (!model_1) {
@@ -99,6 +119,7 @@ int main(int argc, char* argv[]) {
                     SIZE_T heap_size = pmc.WorkingSetSize; // Heap size
                     std::cout << "Commit Size: " << commit_size / (1024 * 1024) << " MB, "
                               << "Heap Size: " << heap_size / (1024 * 1024) << " MB" << std::endl;
+                    csv_file << i << "," << commit_size / (1024 * 1024) << "," << heap_size / (1024 * 1024) << std::endl;
                 }
 #endif
             }
@@ -108,8 +129,9 @@ int main(int argc, char* argv[]) {
         double fps = niters / total_duration.count();
         std::cout << "Inference completed. " << "FPS: " << fps << " Total Time: " << total_duration.count()
                   << " seconds" << std::endl;
-
+        csv_file.close();
     } catch (const std::exception& ex) {
+        csv_file.close();
         std::cerr << ex.what() << std::endl;
         return EXIT_FAILURE;
     }
